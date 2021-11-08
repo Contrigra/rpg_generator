7b6ae052148c162076e1f32555ae371781609856
Contrigra <gaponov60b@gmail.com>
2021-11-08 18:12:16 +0300

    feat: created proper routing for knave's character generator

--- knave/views.py
+++ knave/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+
+def generate_character(request):
+
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
