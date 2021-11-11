5d06bd59c2c3b8bf0144724efc556a92bb40d44f
Contrigra <gaponov60b@gmail.com>
2021-11-11 03:48:36 +0300

    created Trait, Name, Item model.
    Added name list input in the command

--- knave/models.py
+++ knave/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# TODO модели персонажей
+# TODO Модели предметов
+# TODO Модели трейтов
+
+
+class Trait(models.Model):
+    type = models.CharField(max_length=50)
+    trait_title = models.CharField(max_length=75)
+
+
+class Name(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Item(models.Model):
+    item_title = models.CharField(max_length=100, unique=True)
+    quantity = models.SmallIntegerField(blank=True)
+    dimension = models.CharField(max_length=30, blank=True)
+
+
