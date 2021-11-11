ae29e9f30a77570c5c2803c4de2bcef0fbf1b778
Contrigra <gaponov60b@gmail.com>
2021-11-12 01:20:15 +0300

    Feat(knave.models): finished trait(with tags), name, item models

--- knave/models.py
+++ knave/models.py
@@ -1,23 +1,20 @@
 from django.db import models
-
-
-# TODO модели персонажей
-# TODO Модели предметов
-# TODO Модели трейтов
+from taggit.managers import TaggableManager
 
 
 class Trait(models.Model):
-    type = models.CharField(max_length=50)
-    trait_title = models.CharField(max_length=75)
+    """A model for Character Traits"""
+    tags = TaggableManager()
+    title = models.CharField(max_length=75)
 
 
 class Name(models.Model):
+    """Simple Character name model"""
     name = models.CharField(max_length=50)
 
 
 class Item(models.Model):
+    """Model of possible items, items can be countable and measurable"""
     item_title = models.CharField(max_length=100, unique=True)
     quantity = models.SmallIntegerField(blank=True)
     dimension = models.CharField(max_length=30, blank=True)
-
-
