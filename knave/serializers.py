2d99cb2b17fbfa83d8098cefd381c1f18f5adffb
Contrigra <gaponov60b@gmail.com>
2021-11-17 21:08:02 +0300

    feat(knave.Character): Can now generate HP for itself and send it in the JSON

--- knave/serializers.py
+++ knave/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from django.db import models
-from rest_framework.fields import DictField, CharField
+from rest_framework.fields import DictField, CharField, IntegerField
 
 
 class CharacterSerializer(serializers.Serializer):
     name = CharField(max_length=50)
+    health = IntegerField()
     stats = DictField()
+
 
     #TODO other fields
