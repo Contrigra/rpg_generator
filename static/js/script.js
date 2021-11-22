470af99805fa06278cdc40d566ff267bb771ee45
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2021-11-22 19:13:23 +0300

    Add an example of data, const, queryselector to name output and check the availability the data in DATA

--- static/js/script.js
+++ static/js/script.js
@@ -2,15 +2,29 @@
 // write for i algho to sort data in them
 // write api request
 
-const url = '';
+let DATA = {
+    name: "Tybalt", 
+    stats: {
+        strength: 4, 
+        dexterity: 3, 
+        constitution: 2, 
+        intelligence: 4, 
+        wisdom: 1, 
+        charisma: 1
+    }
+};
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', url, false);
-xhr.send();
-let DATA = JSON.parse(xhr.responseText);
+// const url = '';
 
-bonusOutputCells = document.querySelectorAll('p[class$="bonus-output"]');
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', url, false);
+// xhr.send();
+// let DATA = JSON.parse(xhr.responseText);
 
-DATA.forEach(ability => {
+const bonusOutputCells = document.querySelectorAll('p[class$="bonus-output"]');
 
-});
+const nameOutput = document.querySelector('.name-container__output');
+
+nameOutput.textContent = DATA.name; 
+
+console.log(DATA.stats.charisma);
