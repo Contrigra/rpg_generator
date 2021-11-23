aeb3ac2211fcd2f52223bcbf92e43b32bc300e2f
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2021-11-23 20:23:11 +0300

    Update an example of json, write for in  loop for sorting the data into the table cells

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,8 @@
 // choose all insert elements
-// write for i algho to sort data in them
 // write api request
 
-let DATA = {
-    name: "Tybalt", 
-    stats: {
-        strength: 4, 
-        dexterity: 3, 
-        constitution: 2, 
-        intelligence: 4, 
-        wisdom: 1, 
-        charisma: 1
-    }
-};
-
+let json = '{"name": "Oria", "health": 6, "stats": {"strength": 1, "dexterity": 1, "constitution": 2, "intelligence": 3, "wisdom": 1, "charisma": 2}, "traits": {"Body": "\u0413\u0440\u043e\u043c\u0430\u0434\u043d\u043e\u0435", "Face": "\u0415\u0445\u0438\u0434\u043d\u043e\u0435", "Clothing": "\u041f\u043e\u0442\u0451\u0440\u0442\u0430\u044f", "Skin": "\u0417\u043b\u043e\u0432\u043e\u043d\u043d\u0430\u044f", "Virtue": "\u0426\u0435\u043b\u0435\u0443\u0441\u0442\u0440\u0435\u043c\u043b\u0451\u043d\u043d\u043e\u0441\u0442\u044c", "Hair": "\u041b\u043e\u043c\u043a\u0438\u0435", "Vice": "\u0410\u0433\u0440\u0435\u0441\u0441\u0438\u0432\u043d\u043e\u0441\u0442\u044c", "Speech": "\u0417\u0430\u0438\u043a\u0430\u044e\u0449\u0430\u044f\u0441\u044f", "Background": "\u041a\u043e\u043d\u0442\u0440\u0430\u0431\u0430\u043d\u0434\u0438\u0441\u0442", "Misfortune": "\u0417\u0430\u043c\u0435\u043d\u0451\u043d"}}';
+let DATA = JSON.parse(json);
 // const url = '';
 
 // let xhr = new XMLHttpRequest();
@@ -21,10 +10,21 @@ let DATA = {
 // xhr.send();
 // let DATA = JSON.parse(xhr.responseText);
 
-const bonusOutputCells = document.querySelectorAll('p[class$="bonus-output"]');
+// const bonusOutputCells = document.querySelectorAll('p[class$="bonus-output"]');
 
 const nameOutput = document.querySelector('.name-container__output');
 
 nameOutput.textContent = DATA.name; 
 
-console.log(DATA.stats.charisma);
+for (key in DATA.stats) {
+    document.querySelector('.'+ key +'-container__bonus-output').textContent = DATA.stats[key];
+}
+
+
+
+
+// for (let i = 0; i < 6; i++) {
+//     bonusOutputCells[i].textContent = DATA.stats.i;
+// }
+
+// bonusOutputCells[0].textContent = DATA.stats.strength;
