df26d225139133e299a54a84e18ad0b43f603d1c
Contrigra <gaponov60b@gmail.com>
2021-11-25 19:46:19 +0300

    fix(knave.Character): fixed Цепь string in items_1

--- knave/views.py
+++ knave/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+
+from django.shortcuts import render
 from .utils import Character
 from knave.serializers import CharacterSerializer
 from django.views.decorators.http import require_http_methods
