c1b4159137af2629eebbe134e57c5763d5e5632b
Contrigra <gaponov60b@gmail.com>
2022-06-17 09:01:37 +0300

    Major(knave.routing): Changed URL to generate character function

--- knave/urls.py
+++ knave/urls.py
@@ -5,7 +5,7 @@ from . import views
 # TODO character generation to the front page
 urlpatterns = [
     path('', views.character_page, name='character_page'),
-    path('create_character/generate', views.generate_character,
+    path('character/generate', views.generate_character,
          name='create_character'),
 
 ]
