b4edd2332afd603a6930535268a5fea41a0989d7
Contrigra <gaponov60b@gmail.com>
2022-07-18 16:09:35 +0300

    Django(settings.static): now serves static if needed (by /static/)

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,24 @@
-const url = 'http://84.201.140.229/knave/character/generate';
+const url = `http://${location.host}/knave/character/generate`;
 
-
-const nameOutput = document.querySelector('.name-container__output');
 const button = document.querySelector('.button');
-const healthOutput = document.querySelector('.armor-container__max-output');
+const nameOutput = document.querySelector('.name-container__output');
+const healthOutput = document.querySelector('.hit-points-container__max-output');
+const armorBonusOutput = document.querySelector('.armor-container__bonus-output');
+const armorDefenseOutput = document.querySelector('.armor-container__defense-output');
+
 
 function insertContent(content) {
-    nameOutput.textContent = content.name; 
+    nameOutput.textContent = content.name;
     healthOutput.textContent = content.health;
     for (key in content.stats) {
-        document.querySelector('.'+ key +'-container__bonus-output').textContent = content.stats[key];
+        document.querySelector('.' + key + '-container__bonus-output').textContent = content.stats[key];
+        document.querySelector('.' + key.toLowerCase() + '-container__defense-output').textContent = content.stats[key] + 10;
     }
     for (key in content.traits) {
         document.querySelector('.' + key.toLowerCase() + '-container__trait-output').textContent = content.traits[key];
     }
+    armorBonusOutput.textContent = content.armor.armor_class - 10;
+    armorDefenseOutput.textContent = content.armor.armor_class;
 }
 
 function getData(event) {
@@ -29,11 +34,7 @@ function getData(event) {
     .catch(error => console.error('There has been a problem with your fetch operation:', error))
 }
 
-button.addEventListener("click", getData);
+window.addEventListener('load', getData);
+button.addEventListener('click', getData);
 
-
-
-
-// Update insertContent() up to new src structure
-// Make sheet cells render on window load
 
