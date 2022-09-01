19a4aecb42292f5c12d1bab151be6207ad47ef9a
Contrigra <gaponov60b@gmail.com>
2022-09-01 16:27:01 +0300

    minor(knave.models): added __str__ representations of Trait and Name models

--- knave/admin.py
+++ knave/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#TODO register Items, Names, Traits
--- knave/models.py
+++ knave/models.py
@@ -2,15 +2,20 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 
+# TODO string representation
 class Trait(models.Model):
     """A model for Character Traits"""
     tags = TaggableManager()
     title = models.CharField(max_length=75)
 
+    def __str__(self):
+        return f'{self.title}'
 
 class Name(models.Model):
     """Simple Character name model"""
     name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Item(models.Model):
