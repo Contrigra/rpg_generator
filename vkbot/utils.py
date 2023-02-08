fea6e93829b56fdc8c30bb576d91f72acb258a3e
Contrigra <gaponov60b@gmail.com>
2023-02-08 12:36:23 +0300

    feat(vk.bot): return knave character as a string and send it to the chat

--- vkbot/utils.py
+++ vkbot/utils.py
@@ -1,4 +1,5 @@
 import requests
+from knave.knave_character import generate_knave_character
 
 
 def get_callback_confirmation_code(access_token,
@@ -13,6 +14,39 @@ def get_callback_confirmation_code(access_token,
 
 
 def get_knave_character():
-    ...
+    pc = generate_knave_character()  # player character
 
-    return 'Temporary Message'
+    pc_auxiliary_items = ''
+    if None not in pc.armor['auxiliary_items']:
+        pc_auxiliary_items = f"{pc.armor['auxiliary_items']}"
+
+    pc_string = f'''
+    Тебя зовут - {pc.name} 
+    HP - {pc.health}
+    
+    Сила - {pc.stats['strength']}     
+    Ловкость - {pc.stats['dexterity']} 
+    Телосложение - {pc.stats['constitution']}
+    Интеллект - {pc.stats['intelligence']}
+    Мудрость - {pc.stats['wisdom']}
+    Харизма - {pc.stats['charisma']}
+    
+    Броня - {pc.armor['armor_class']}
+    Предметы брони: {pc.armor['body']} {pc_auxiliary_items}
+    
+    Инвентарь: {pc.inventory}
+    
+    Особенности:
+    Тело - {pc.traits['body']}
+    Лицо - {pc.traits['face']}
+    Одежда - {pc.traits['clothing']}
+    Кожа - {pc.traits['skin']}
+    Волосы - {pc.traits['hair']}
+    Порок - {pc.traits['vice']}
+    Добродетель - {pc.traits['virtue']}
+    Речь  - {pc.traits['speech']}
+    Прошлое - {pc.traits['background']}
+    Неудача - {pc.traits['misfortune']}
+    '''
+
+    return pc_string
