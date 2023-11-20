5122711bffa8c87654d70357dc7d240a1163f2b1
Contrigra <gaponov60b@gmail.com>
2023-11-20 14:29:07 +0300

    refactor(content render): split spaghetti code into many small functions

--- static/js/script.js
+++ static/js/script.js
@@ -1,48 +1,6 @@
 const url = `${location.protocol}//${location.host}/knave/character/generate/`;
 const button = document.querySelector('.button');
 
-
-function insertContent(content) {
-
-    const nameOutput = document.querySelector('.name-container__output');
-    const healthOutput = document.querySelector('.hit-points-container__max-output');
-    const armorBonusOutput = document.querySelector('.armor-container__bonus-output');
-    const armorDefenseOutput = document.querySelector('.armor-container__defense-output');
-    const inventoryOutput = document.querySelector('.items-container__item-output');
-    const armorItemsOutput = document.querySelector('.armor-container__item-output');
-
-    nameOutput.textContent = content.name;
-    healthOutput.textContent = content.health;
-
-    // bonus and defense render
-    for (key in content.stats) {
-        document.querySelector('.' + key + '-container__bonus-output').textContent = content.stats[key];
-        document.querySelector('.' + key.toLowerCase() + '-container__defense-output').textContent = content.stats[key] + 10;
-    }
-
-    // trait render
-    for (key in content.traits) {
-        document.querySelector('.' + key.toLowerCase() + '-container__trait-output').textContent = content.traits[key];
-    }
-
-    // armor render
-    armorBonusOutput.textContent = content.armor.armor_class - 10;
-    armorDefenseOutput.textContent = content.armor.armor_class;
-
-    armorItemsOutput.textContent = '' // if I did not clear the content, it would save previous result and get stacked
-    armorItemsOutput.insertAdjacentHTML("afterbegin", `<ul>${content.armor.body}</ul>`);
-    if (content.armor.auxiliary_items[0] !== null) {
-        armorItemsOutput.insertAdjacentHTML("afterbegin", `<ul>${content.armor.auxiliary_items}</ul>`);
-    }
-
-    inventoryOutput.textContent = ''
-    for (let i = 0; i < content.inventory.length; i++) {
-        inventoryOutput.insertAdjacentHTML("afterbegin", `<ul>${content.inventory[i]}</ul>`);
-    }
-
-
-}
-
 function getData(event) {
     event.preventDefault();
     fetch(url)
@@ -60,3 +18,63 @@ window.addEventListener('load', getData);
 button.addEventListener('click', getData);
 
 
+function insertContent(data) {
+    const {inventory, stats, armor, name, health, traits} = data;
+
+    nameRender(name);
+    healthRender(health);
+    bonusDefenseRender(stats);
+    traitRender(traits);
+    armorStatRender(armor);
+    armorItemRender(armor, inventory);
+
+}
+
+function nameRender(name) {
+    const nameOutput = document.querySelector('.name-container__output');
+    nameOutput.textContent = name;
+}
+
+function healthRender(health) {
+    const healthOutput = document.querySelector('.hit-points-container__max-output');
+    healthOutput.textContent = health;
+
+}
+
+function armorItemRender(armor, inventory) {
+    const inventoryOutput = document.querySelector('.items-container__item-output');
+    const armorItemsOutput = document.querySelector('.armor-container__item-output');
+
+    armorItemsOutput.textContent = '' // if I did not clear the content, it would save previous result and get stacked
+    armorItemsOutput.insertAdjacentHTML("afterbegin", `<ul>${armor.body}</ul>`);
+    if (armor.auxiliary_items[0] !== null) {
+        armorItemsOutput.insertAdjacentHTML("afterbegin", `<ul>${armor.auxiliary_items}</ul>`);
+    }
+    inventoryOutput.textContent = ''
+    for (let i = 0; i < inventory.length; i++) {
+        inventoryOutput.insertAdjacentHTML("afterbegin", `<ul>${inventory[i]}</ul>`);
+    }
+
+}
+
+function bonusDefenseRender(stats) {
+    for (key in stats) {
+        document.querySelector('.' + key + '-container__bonus-output').textContent = stats[key];
+        document.querySelector('.' + key.toLowerCase() + '-container__defense-output').textContent = stats[key] + 10;
+    }
+
+}
+
+function armorStatRender(armor) {
+    const armorBonusOutput = document.querySelector('.armor-container__bonus-output');
+    const armorDefenseOutput = document.querySelector('.armor-container__defense-output');
+
+    armorBonusOutput.textContent = armor.armor_class - 10;
+    armorDefenseOutput.textContent = armor.armor_class;
+}
+
+function traitRender(traits) {
+    for (key in traits) {
+        document.querySelector('.' + key.toLowerCase() + '-container__trait-output').textContent = traits[key];
+    }
+}
